c523e9958c6f7ef63b9f49408121111eaf17f9d1
Monika Gębołyś <monpas@wp.pl>
2019-01-26 14:34:59 +0100

    Select course

--- helloworldserlvet/src/main/java/pl/sdacademy/HelloWorldServlet.java
+++ helloworldserlvet/src/main/java/pl/sdacademy/HelloWorldServlet.java
@@ -1,4 +1,29 @@
 package pl.sdacademy;
 
-public class HelloWorldServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+@WebServlet(name = "HelloWorldServlet", value = "/helloWorld")
+public class HelloWorldServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<!DOCTYPE html>\n" +
+                "<head> \n" +
+                "    <title>Dynamiczna strona www</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "Dzisiaj jest " + LocalDateTime.now() + "\n" +
+                "</body>\n" +
+                "</html>");
+    }
 }
